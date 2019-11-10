f5300b8362ccad0f0fbc71f98081dc7a6dceeea1
soramugi <soramugi.chika@gmail.com>
2019-11-10 20:51:23 +0900

    macアプリケーションで終了できるように

--- src/background.ts
+++ src/background.ts
@@ -41,9 +41,9 @@ function createWindow() {
     win = null;
   });
 
-  const menu = Menu.buildFromTemplate([
+  const template: any = [
     {
-      label: 'inco',
+      label: 'Edit',
       submenu: [
         {
           label: 'ダウンロード先の変更',
@@ -63,12 +63,84 @@ function createWindow() {
           },
         },
         { type: 'separator' },
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        // {role: 'toggledevtools'},
+        // {type: 'separator'},
+        // {role: 'resetzoom'},
+        // {role: 'zoomin'},
+        // {role: 'zoomout'},
+        { role: 'togglefullscreen' },
+        { type: 'separator' },
+      ],
+    },
+    {
+      role: 'window',
+      submenu: [{ role: 'minimize' }, { role: 'close' }],
+    },
+    {
+      role: 'version',
+      submenu: [
         {
           label: 'Version: ' + app.getVersion(),
         },
       ],
     },
-  ]);
+  ];
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' },
+      ],
+    });
+
+    // Edit menu
+    // template[1].submenu.push(
+    //   {type: 'separator'},
+    //   {
+    //     label: 'Speech',
+    //     submenu: [
+    //       {role: 'startspeaking'},
+    //       {role: 'stopspeaking'}
+    //     ]
+    //   }
+    // )
+
+    // Window menu
+    template[3].submenu = [
+      { role: 'close' },
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { type: 'separator' },
+      { role: 'front' },
+    ];
+  }
+
+  const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 
   win.on('resize', () => {
